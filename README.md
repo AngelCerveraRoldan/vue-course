11aeebae9ed53e2bad28ae1a8485764d79433a1b
Angel Cervera Roldan <angelcerveraroldan@protonmail.com>
2022-07-20 10:16:06 +0100

    hello world app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Vue udemy course
+# Vue 3 udemy [course](https://www.udemy.com/course/complete-vue-js-developer-zero-to-mastery-vuex/learn/lecture/25363430#overview)
+
+## What is thought in the course
+- Fundamentals
+  - Project One, Perspective playground
+- Vue Developer environment
+  1. Sass
+- Animations and transitions
+- Project 2, quiz app
+- Main project, music player
--- hello-world/app.js
+++ hello-world/app.js
@@ -1,15 +1,41 @@
-Vue.createApp({
+// vm stands for view model
+const vm = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredValue: "",
+            firstName: "Angel",
+            lastName: "Cervera",
+            age: 20,
+            isPurple: false,
+            selectedColor: ""
         };
     },
-
     methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue)
-            this.enteredValue = ""
+        increment() {
+            this.age++
+        },
+
+        updateLastName(msg, event) {
+            console.log(msg)
+            this.lastName = event.target.value
+        },
+
+        changeColor() { this.isPurple = !this.isPurple }
+    },
+    computed: {
+        fullName() {
+            return `${this.firstName} ${this.lastName}`
+        },
+
+        colorClasses() {
+            return {
+                purple: this.isPurple
+            }
         }
     }
-}).mount("#app");
+}).mount("#app")
+
+setTimeout(() => {
+    vm.firstName = "Pilar"
+}, 2000)
+
+
